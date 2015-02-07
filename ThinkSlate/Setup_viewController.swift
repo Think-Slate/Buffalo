d0afebd3ca8b5d344724ac2424821fcc17c8b681
Victor Vainberg <vctrvainberg@gmail.com>
2015-02-07 12:20:13 -0500

    changed name

---
+++ ThinkSlate/Setup_viewController.swift
@@ -0,0 +1,9 @@
+//
+//  Setup_viewController.swift
+//  ThinkSlate
+//
+//  Created by Victor Vainberg on 2/7/15.
+//  Copyright (c) 2015 Victor Vainberg. All rights reserved.
+//
+
+import Foundation
