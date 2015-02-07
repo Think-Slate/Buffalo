d6cf1eb5764f107ffc4cb2c8ab9a390338d440f5
Victor Vainberg <vctrvainberg@gmail.com>
2015-02-07 12:21:13 -0500

    removed thinkslate junk

--- ThinkSlate/Setup_viewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Setup_viewController.swift
-//  ThinkSlate
-//
-//  Created by Victor Vainberg on 2/7/15.
-//  Copyright (c) 2015 Victor Vainberg. All rights reserved.
-//
-
-import Foundation
