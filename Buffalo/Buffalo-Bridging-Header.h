d08fc098a423269d7f41d64f6fc2f47a5fe15461
Victor Vainberg <vctrvainberg@gmail.com>
2015-03-07 19:24:46 -0500

    cleaning buffalo project, readying for bond

--- Buffalo/Buffalo-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-//
-//  Prefix header
-//
-//  The contents of this file are implicitly included at the beginning of every source file.
-//
-
